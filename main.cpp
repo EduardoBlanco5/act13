3254b0f4fb108b906558606a4ba40fecfdcc2a77
Eduardo Blanco <eduardo.blanco0841@alumnos.udg.mx>
2020-11-03 20:03:27 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "ArregloDinamico.h"
+
+using namespace std;
+
+int main()
+{
+
+    ArregloDinamico<string> arreglo;
+
+    arreglo.insertar_final("P"); 
+    arreglo.insertar_final("A"); 
+   
+    arreglo.insertar_inicio("O"); 
+    arreglo.insertar_inicio("H"); 
+    arreglo.insertar_final("S");
+
+    for(size_t i = 0; i< arreglo.size(); i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+    cout <<"\n"<<endl;
+    arreglo.insertar("L", 2);
+    cout << endl;
+    for(size_t i = 0; i< arreglo.size(); i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+    cout <<"\n"<<endl;
+    arreglo.eliminar_inicio();
+    arreglo.eliminar_final();
+    for(size_t i = 0; i< arreglo.size(); i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+    cout <<"\n"<<endl;
+
+    arreglo.eliminar(1);
+    for(size_t i = 0; i< arreglo.size(); i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+    cout <<"\n"<<endl;
+
+    
+    
+    system("pause");
+    return 0;
+}
